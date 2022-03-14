d07434738761f756c10c573eb8ffb51df8e9cdd1
AlexaGTorres <90784633+AlexaGTorres@users.noreply.github.com>
2022-03-13 18:51:32 -0700

    Update Farmer.java

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,3 +1,14 @@
 public class Farmer
 {
+  private final String name;
+
+    public Farmer (String name)
+    {
+        this.name = name;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
 }//End class Farmer

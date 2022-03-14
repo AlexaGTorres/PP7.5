341899221cce091f84817ddcd3b94496e0101b75
AlexaGTorres <90784633+AlexaGTorres@users.noreply.github.com>
2022-03-13 18:50:45 -0700

    Update animalDriver.java

--- src/animalDriver.java
+++ src/animalDriver.java
@@ -1,19 +1,32 @@
 public class animalDriver
 {
     public static void main (String[] args)
-    {
-        //One cow, one pig, two chickens
-        Animal cow = new Animal("C", 2, "cow");
-        Animal pig = new Animal("P", 4, "pig");
-        Animal chick1 = new Animal("C", 0, "chick");
-        Animal chick2 = new Animal("Ch", 3, "chick");
+   {
+       //Farmer, one cow, one pig, two chicks
 
-        //Farmer
-        //Create Farm
-        Animal[] Farm = new Animal[4];
-        Farm[0] = cow;
-        Farm[1] = pig;
-        Farm[2] = chick1;
-        Farm[3] = chick2;
-    }
+       Farmer farmer  = new Farmer ("Old MacDonald");
+
+       Animal cow = new Cow ("John", "cow", "moo");
+
+       Animal pig = new Pig ("Oscar", "pig", "oink");
+
+       Animal chick = new Chick ("Peach", "chick", "peep", 0, true, false);
+       Animal chickFriend = new Chick ("Bee", "chick", "cheep",3, false, true);
+
+       Animal[] myFarm = new Animal[4];
+       myFarm[0] = cow;
+       myFarm[1] = pig;
+       myFarm[2] = chick;
+       myFarm[3] =  chickFriend;
+
+       for (Animal element : myFarm)
+       {
+           System.out.print (farmer.getName() + " had a farm, E-I-E-I-O! " +
+                   "\nAnd on that farm he had a " + element.getType() + ", E-I-E-I-O!" +
+                   "\nWith a " + element.getSound() + "-" + element.getSound() + " here and a " + element.getSound() + "-" + element.getSound() + " there," +
+                   "\nHere a " + element.getSound() + ", there a " + element.getSound() + ", everywhere " + element.getSound() + "-" + element.getSound() + "" +
+                   "\n" + farmer.getName() + " had a farm, E-I-E-I-O!");
+           System.out.println ("\n");
+       }
+   }
 }//End animalDriver class

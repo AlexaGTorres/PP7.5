5c7af772e4f6ea5a54c73461222d553f8c20bc46
AlexaGTorres <90784633+AlexaGTorres@users.noreply.github.com>
2022-03-15 20:25:10 -0700

    Update Farmer.java

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,6 +1,6 @@
 public class Farmer
 {
-  private final String name;
+ private final String name;
 
     public Farmer (String name)
     {
@@ -10,5 +10,16 @@ public class Farmer
     public String getName ()
     {
         return name;
+    }
+
+    public String toString (Animal i)
+    {
+        String output = name + " had a farm, E-I-E-I-O!" +
+                "\nAnd on that farm he had a " + i.getType() + ", E-I-E-I-O!" +
+                "\nWith a " + i.getSound() + "-" + i.getSound() + "here, " +
+                "and a " + i.getSound() + "-" + i.getSound() + " there, " +
+                "\nHere a " + i.getSound() + ", there a " + i.getSound() + " everywhere a " + i.getSound() + "-" + i.getSound() +
+                "\n" + name + " had a farm, E-I-E-I-O!";
+        return output;
     }
 }//End class Farmer
